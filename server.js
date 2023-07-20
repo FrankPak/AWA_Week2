0e3d576a5b2e36378cee541cb02d2bb8659f1a0c
FrankPak <francois.pakkanen@gmail.com>
2023-07-21 02:20:43 +0300

    added static, working on task 5

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const os = require("os");
 const app = express();
 const port = 3000;
 
+app.use(express.static('static'))
+
 app.get("/hello", (req,res) => {
     res.json({msg:"Hello World"})
 
@@ -14,13 +16,16 @@ app.get("/echo/:id", (req,res) => {
 });
 
 app.post("/sum", (req, res) => {
-    let sum = 0;
     res.json({sum:req.body.numbers.reduce(myFunc)})
 })
 
 function myFunc(total, num) {
     return total = total + num;
 }
+
+app.post("/list", (req, res) => {
+    
+})
 
 app.listen(port, () => console.log(`Server listening a port ${port}!`));
 

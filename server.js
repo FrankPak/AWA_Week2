fd0f5e379cd39ace8ae0a55f5e1f5878b65c853b
Francois Pakkanen <francois.pakkanen@gmail.com>
2024-07-13 18:42:40 +0300

    made cleaner and got sum working

--- server.js
+++ server.js
@@ -2,26 +2,25 @@ const express = require("express");
 const os = require("os");
 const app = express();
 const port = 3000;
+const initalValue = 0
 
+app.use(express.json())
 app.use(express.static('static'))
 
 app.get("/hello", (req,res) => {
-    res.json({msg:"Hello World"})
+    res.json({msg:"Hello World"});
 
 });
 
 app.get("/echo/:id", (req,res) => {
-    res.json({id:req.params.id})
+    res.json({id:req.params.id});
 
 });
 
 app.post("/sum", (req, res) => {
-    res.json({sum:req.Body.numbers.reduce(myFunc)})
+    res.json({sum:req.body.numbers.reduce((accumulator, currentValue) => accumulator + currentValue, initalValue)}) //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 })
 
-function myFunc(total, num) {
-    return total = total + num;
-}
 
 app.post("/list", (req, res) => {
     
